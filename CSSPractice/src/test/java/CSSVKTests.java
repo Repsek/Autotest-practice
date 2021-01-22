740c41913ea093c406a73867bc297394640bcdfd
Repsek <ianenkow@gmail.com>
2021-01-22 17:11:20 +0300

    Test commit to CSS branch 1

--- CSSPractice/src/test/java/CSSVKTests.java
+++ CSSPractice/src/test/java/CSSVKTests.java
@@ -1,2 +1,10 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class CSSVKTests {
+    private static WebDriver driver;
+    private static WebDriverWait wait;
+
+   
 }
